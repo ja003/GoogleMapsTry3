f77ad2ed3b1d191a1e28759629cc4facfbfd4d4e
Adam Jurík <ja004@seznam.cz>
2019-02-27 15:50:10 +0100

    Maps initialization

--- GoogleMapsTry3/GoogleMapsTry3.Wpf/MainWindow.xaml.cs
+++ GoogleMapsTry3/GoogleMapsTry3.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.WPF;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.WPF;
+using Xamarin;
 
 namespace GoogleMapsTry3.Wpf
 {
@@ -14,6 +17,16 @@ namespace GoogleMapsTry3.Wpf
 				InitializeComponent();
 
 				Forms.Init();
+
+				//key: AIzaSyCAIDuaANxKVQoyWXlf2hU_BxGT1ldJQUg
+				//Xamarin.Forms.Maps.Map( .Forms. Maps.Init("AIzaSyCAIDuaANxKVQoyWXlf2hU_BxGT1ldJQUg");
+
+				//Xamarin.Forms.Maps.WPF.MapRenderer.
+				//MapRenderer m = new MapRenderer();
+				//m.Control.ke
+				FormsMaps.Init("AIzaSyCAIDuaANxKVQoyWXlf2hU_BxGT1ldJQUg");
+
+
 				LoadApplication(new GoogleMapsTry3.App());
 		  }
 	 }
--- GoogleMapsTry3/GoogleMapsTry3/MainPage.xaml.cs
+++ GoogleMapsTry3/GoogleMapsTry3/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace GoogleMapsTry3
 {
@@ -11,7 +12,21 @@ namespace GoogleMapsTry3
 	 {
 		  public MainPage()
 		  {
-				InitializeComponent();
+
+				var map = new Map(
+				MapSpan.FromCenterAndRadius(
+						  new Position(37, -122), Distance.FromMiles(0.3)))
+				{
+					 IsShowingUser = true,
+					 HeightRequest = 100,
+					 WidthRequest = 960,
+					 VerticalOptions = LayoutOptions.FillAndExpand
+				};
+				var stack = new StackLayout { Spacing = 0 };
+				stack.Children.Add(map);
+				Content = stack;
+
+				//InitializeComponent();
 		  }
 	 }
 }
