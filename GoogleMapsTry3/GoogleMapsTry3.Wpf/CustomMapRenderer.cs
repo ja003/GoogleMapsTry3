864da3e96baf388711d337df7ed33de2db5c13bc
Adam Jurík <ja004@seznam.cz>
2019-02-28 12:15:19 +0100

    MapRenderer WPF - FAIL

---
+++ GoogleMapsTry3/GoogleMapsTry3.Wpf/CustomMapRenderer.cs
@@ -0,0 +1,31 @@
+﻿using GoogleMapsTry3;
+using GoogleMapsTry3.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.WPF;
+using Xamarin.Forms.Platform.WPF;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
+namespace GoogleMapsTry3.Wpf
+{
+	 class CustomMapRenderer : MapRenderer
+	 {
+		  Microsoft.Maps.MapControl.WPF.Map nativeMap;
+		  List<CustomPin> customPins;
+
+		  protected override void OnElementChanged(ElementChangedEventArgs<Map> e)
+		  {
+				base.OnElementChanged(e);
+
+				if(e.OldElement != null)
+				{
+
+					 //nativeMap.InfoWindowClick -= OnInfoWindowClick;
+				}
+		  }
+	 }
+}
